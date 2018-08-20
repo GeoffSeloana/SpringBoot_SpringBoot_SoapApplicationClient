65b5629379ddf8789f3149ac3a1ede7e93942252
Geoffrey Seloana <geoff.seloana@gmail.com>
2018-08-20 19:04:04 +0200

    initial commit

---
+++ src/main/java/mynewnewpackage/ObjectFactory.java
@@ -0,0 +1,112 @@
+
+package mynewnewpackage;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the mynewnewpackage package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: mynewnewpackage
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link CIupdRefreshClientV1 }
+     * 
+     */
+    public CIupdRefreshClientV1 createCIupdRefreshClientV1() {
+        return new CIupdRefreshClientV1();
+    }
+
+    /**
+     * Create an instance of {@link CIupdRefreshClientV1Response }
+     * 
+     */
+    public CIupdRefreshClientV1Response createCIupdRefreshClientV1Response() {
+        return new CIupdRefreshClientV1Response();
+    }
+
+    /**
+     * Create an instance of {@link CIupdRefreshClientV1Response.Nbsmsgo3 }
+     * 
+     */
+    public CIupdRefreshClientV1Response.Nbsmsgo3 createCIupdRefreshClientV1ResponseNbsmsgo3() {
+        return new CIupdRefreshClientV1Response.Nbsmsgo3();
+    }
+
+    /**
+     * Create an instance of {@link CIupdRefreshClientV1 .Nbsmsgi }
+     * 
+     */
+    public CIupdRefreshClientV1 .Nbsmsgi createCIupdRefreshClientV1Nbsmsgi() {
+        return new CIupdRefreshClientV1 .Nbsmsgi();
+    }
+
+    /**
+     * Create an instance of {@link CIupdRefreshClientV1 .Nbsapdpi }
+     * 
+     */
+    public CIupdRefreshClientV1 .Nbsapdpi createCIupdRefreshClientV1Nbsapdpi() {
+        return new CIupdRefreshClientV1 .Nbsapdpi();
+    }
+
+    /**
+     * Create an instance of {@link CIupdRefreshClientV1 .Cip042I }
+     * 
+     */
+    public CIupdRefreshClientV1 .Cip042I createCIupdRefreshClientV1Cip042I() {
+        return new CIupdRefreshClientV1 .Cip042I();
+    }
+
+    /**
+     * Create an instance of {@link CIupdRefreshClientV1Response.Nbsapdpo }
+     * 
+     */
+    public CIupdRefreshClientV1Response.Nbsapdpo createCIupdRefreshClientV1ResponseNbsapdpo() {
+        return new CIupdRefreshClientV1Response.Nbsapdpo();
+    }
+
+    /**
+     * Create an instance of {@link CIupdRefreshClientV1Response.Cip042O }
+     * 
+     */
+    public CIupdRefreshClientV1Response.Cip042O createCIupdRefreshClientV1ResponseCip042O() {
+        return new CIupdRefreshClientV1Response.Cip042O();
+    }
+
+    /**
+     * Create an instance of {@link CIupdRefreshClientV1Response.Nbsmsgo3 .MsgEntry }
+     * 
+     */
+    public CIupdRefreshClientV1Response.Nbsmsgo3 .MsgEntry createCIupdRefreshClientV1ResponseNbsmsgo3MsgEntry() {
+        return new CIupdRefreshClientV1Response.Nbsmsgo3 .MsgEntry();
+    }
+
+    /**
+     * Create an instance of {@link CIupdRefreshClientV1 .Nbsmsgi.FinalMsgId }
+     * 
+     */
+    public CIupdRefreshClientV1 .Nbsmsgi.FinalMsgId createCIupdRefreshClientV1NbsmsgiFinalMsgId() {
+        return new CIupdRefreshClientV1 .Nbsmsgi.FinalMsgId();
+    }
+
+}
